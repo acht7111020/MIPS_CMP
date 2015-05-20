82450f1ddb4970f012e94a7cdddc6942e39c8f23
acht7111020 <ciindy321123@yahoo.com.tw>
2015-05-21 00:05:49 +0800

    Initial Commit

---
+++ project3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project3
+//
+//  Created by Blurry on 2015/5/21.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
